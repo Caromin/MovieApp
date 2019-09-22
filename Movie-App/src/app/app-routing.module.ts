353d8059dfb6f1d8ef6c5ac36ad0b6bc311b55c9
Narely Najera <nnarely@yahoo.com>
2019-09-22 11:57:07 -0400

    navbar links work

--- Movie-App/src/app/app-routing.module.ts
+++ Movie-App/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: '', component: HomepageComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent},
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
